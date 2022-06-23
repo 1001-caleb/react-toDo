81001da7a899fbd4ada8ca3434c37c8f35960648
1001-caleb <mccalebyt@gmail.com>
2022-06-23 08:52:15 -0500

    input task creator styled

--- src/components/TaskCreator.jsx
+++ src/components/TaskCreator.jsx
@@ -14,12 +14,13 @@ export const TaskCreator = ({ createNewTask }) => {
         <form onSubmit={handleSubmit}>
             {/* capturando lo que typeamos en el input con useState  */}
             <input
+                className='input input-bordered w-60 md:w-96'
                 type="text"
                 placeholder='Enter a new task'
                 value={newTaskName}
                 onChange={(e) => setNewTaskName(e.target.value)}
             />
-            <button onClick={(handleSubmit)}>save task</button>
+            <button className='btn btn-square' onClick={(handleSubmit)}>save</button>
         </form>
     )
 
