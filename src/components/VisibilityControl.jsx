b8bbacec0630a9d06cd7fdcc51f394792fce8c2e
1001-caleb <mccalebyt@gmail.com>
2022-06-23 09:46:41 -0500

    task completed styles

--- src/components/VisibilityControl.jsx
+++ src/components/VisibilityControl.jsx
@@ -1,23 +1,30 @@
 export const VisibilityControl = ({ setShowCompleted, cleanTask, isChecked }) => {
+
+
     const handleDelete = () => {
-        if (window.confirm('are you sure want to delete it?')){
+
+        if (window.confirm('are you sure want to delete it?')) {
             cleanTask();
             setShowCompleted(false)
         }
+
     }
 
     return (
-        <div>
+        <div className='flex gap-4 justify-between p-4 '>
             <input
+                className='checkbox'
                 checked={isChecked}
                 type="checkbox"
                 onChange={(e) => setShowCompleted(e.target.checked)}
             />{''}
             <label>Show task done</label>
 
-            <button onClick={handleDelete}>
+            <button onClick={handleDelete} className='text-xl font-extrabold'>
                 X
             </button>
+
+
         </div>
     )
 }
